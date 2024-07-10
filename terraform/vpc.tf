86351da264c5ca4e117c706bd9cf96748590bafb
Aliza21tkxel <aliza.waseem@camp2.tkxel.com>
2024-07-11 03:43:42 +0500

    Testing docker image

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "Terraform-ec2" {
   ami           = "ami-0c02fb55956c7d316"  # Replace with your desired AMI
   instance_type = "t3.micro"
-  subnet_id=aws_subnet.public.id
+  subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   # key_name      = "my-terraform-key"     # Name of the key pair created in AWS 
   tags = {
