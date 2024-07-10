701aa3ca389f16592e4557529c991871db4c8c21
Aliza21tkxel <aliza.waseem@camp2.tkxel.com>
2024-07-10 13:36:23 +0500

    Changes in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test('renders form heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Form in React/i);
+  expect(headingElement).toBeInTheDocument();
 });
