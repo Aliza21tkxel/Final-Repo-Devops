e7e0ca7960228e34b97c265421ea0d6c6bf83d39
Aliza21tkxel <aliza.waseem@camp2.tkxel.com>
2024-07-10 13:00:03 +0500

    Update locals.tf

---
+++ terraform/locals.tf
@@ -0,0 +1,11 @@
+# locals.tf
+
+locals {
+  owners      = var.owners
+  environment = var.environment
+  name        = "${local.owners}-${local.environment}"
+  tags = {
+    owners      = local.owners
+    environment = local.environment
+  }
+}
